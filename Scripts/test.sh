0863bcccadeb8fe0d909b9e464c1ff1a4bc71175
Edik Simonian <edisimon@hotmail.com>
2017-05-03 22:16:20 -0700

    Add debug code to fix the build

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until $(curl --output /dev/null --silent --head --fail -H 'Host: example.com' http://127.0.0.1:80); do printf '.'; sleep 5; done
+until $(curl --output /dev/null --silent --head --fail -H 'Host: example.com' http://127.0.0.1:80); do docker ps; sleep 5; done
 
 curl --verbose -H 'Host: example.com' 'http://127.0.0.1:80/'
 
